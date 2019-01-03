26278dc4951a0b0e8ff4c499c8885de9f4044813
Mohamad Rizki Dafianto <mohamad.rizki.dafianto@gmail.com>
2019-01-03 14:53:25 +0700

    Add several entity

---
+++ app/src/main/java/com/dafian/android/dragooners/domain/entity/Weapon.kt
@@ -0,0 +1,18 @@
+package com.dafian.android.dragooners.domain.entity
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class Weapon(
+    @JsonProperty("id") val id: Int,
+    @JsonProperty("slug") val slug: String,
+    @JsonProperty("name") val name: String,
+    @JsonProperty("type") val type: String,
+    @JsonProperty("rarity") val rarity: Int,
+    @JsonProperty("attack") val attack: Any,
+    @JsonProperty("slots") val slotList: List<Slot>,
+    @JsonProperty("elements") val elementList: List<Any>,
+    @JsonProperty("crafting") val crafting: Any,
+    @JsonProperty("assets") val assets: Any,
+    @JsonProperty("sharpness") val sharpness: Any,
+    @JsonProperty("durability") val durability: Any
+)
--- app/src/main/java/com/dafian/android/dragooners/util/EnumType.kt
+++ app/src/main/java/com/dafian/android/dragooners/util/EnumType.kt
@@ -12,3 +12,20 @@ enum class ArmorType {
     WAIST,
     LEGS
 }
+
+enum class WeaponType {
+    GREAT_SWORD,
+    LONG_SWORD,
+    SWORD_AND_SHIELD,
+    DUAL_BLADES,
+    HAMMER,
+    HUNTING_HORN,
+    LANCE,
+    GUN_LANCE,
+    SWITCH_AXE,
+    CHANGE_BLADE,
+    INSECT_GLAIVE,
+    LIGHT_BOW_GUN,
+    HEAVY_BOW_GUN,
+    BOW
+}
